3f0cd21ff1906c8803f154c83f854e2c7a821443
Florian Paul Schmidt <florian_paul.schmidt@uni-bielefeld.de>
2019-04-03 14:59:43 +0200

    - oops

--- clustering.jl
+++ clustering.jl
@@ -34,7 +34,7 @@ end
 function clustering(idxs, centralities)
     assignments = clustering_step(idxs, centralities)
     
-    max_step = 100
+    # max_step = 100
     
     step = 1
     while true
@@ -42,23 +42,23 @@ function clustering(idxs, centralities)
         new_assignments = copy(assignments[:, step])
         # display(new_assignments)
         
-        #for index = 1:size(idxs,1)
-        #    old = old_assignments[index]
-        #    new = old_assignments[old]
-        #    new_assignments[index] = new
-        #end
+        # for index = 1:size(idxs,1)
+        #     old = old_assignments[index]
+        #     new = old_assignments[old]
+        #     new_assignments[index] = new
+        # end
         
         new_assignments = old_assignments[old_assignments]
         
-        #display(new_assignments == old_assignments)
+        # display(new_assignments == old_assignments)
         
         if new_assignments == old_assignments
             break
         end
         
-        if step > max_step
-            break
-        end
+        # if step > max_step
+        #     break
+        # end
         
         assignments = hcat(assignments, new_assignments)
         
